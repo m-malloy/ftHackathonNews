b6959bef8c307e55bbca3fe92a2d0c7ef22ca644
Nikolay Stoev <nikolaystoev@Nikolays-MacBook-Pro.local>
2017-07-08 18:23:11 +0100

    fix return vales

--- contract/contract.sol
+++ contract/contract.sol
@@ -19,10 +19,11 @@ contract News {
         contractMaker = msg.sender;
     }
     
-    function add(bytes fingerprint, uint price) {
+    function add(bytes fingerprint, uint price) returns (bytes _fingerprint) {
         Content content = contents[fingerprint];
         content.owner = msg.sender;
         content.price = price;
+        return fingerprint;
     }
         
     function getPrice(bytes fingerprint) public constant returns(uint _price){
@@ -30,7 +31,7 @@ contract News {
         return (content.price);
     }
     
-    function buy(bytes fingerprint) payable {
+    function buy(bytes fingerprint) payable returns(bytes _fingerprint){
         Content content = contents[fingerprint];
         Order order = orders[fingerprint];
         if (msg.value < content.price) {
@@ -38,20 +39,17 @@ contract News {
         } else {
             order.buyer = msg.sender;
             content.owner.transfer(msg.value);
+            return fingerprint;
         }   
     }
     
     function proveBuy(bytes fingerprint) public constant returns (bool){
         Order order = orders[fingerprint];
-        if(order.buyer == msg.sender){
-            return true;
-        }
+        return order.buyer == msg.sender;
     }
     
     function proveOwnership(bytes fingerprint) public constant returns (bool){
         Content content = contents[fingerprint];
-        if(content.owner == msg.sender){
-            return true;
-        }
+        return content.owner == msg.sender;
     }
 }
